6c1650cf6ffe01eee5bd3efb2634ac6b94d36af7
Greg Morgan <gregm9999@gmail.com>
2018-11-02 22:50:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def oxford_comma(array)
-
+  if array.length==1
+     array.join
+  elsif array.length==2
+     array.join(" and ")
+  elsif
+    array[-1]="and #{array[-1]}"
+    array.join(", ")
+  end
 end
