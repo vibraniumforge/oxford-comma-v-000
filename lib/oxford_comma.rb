feebf22dc434e632b86a9cbde3a4410c0b2abde1
Greg Morgan <gregm9999@gmail.com>
2018-11-03 00:04:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,19 @@
-require 'pry'
+# require 'pry'
+# def oxford_comma(array)
+#   if array.length==1
+#      array.join
+#   elsif array.length==2
+#      array.join(" and ")
+#   elsif
+#     array[-1]="and #{array[-1]}"
+#     array.join(", ")
+#   end
+# end
 def oxford_comma(array)
-  if array.length==1
-     array.join
-  elsif array.length==2
-     array.join(" and ")
-  elsif
-    array[-1]="and #{array[-1]}"
-    array.join(", ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(1, "and ")
   end
+  array.join(", ")
 end
